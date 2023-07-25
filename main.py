22ba3f806bfddaf1a715a5bf2ef38f71fd6eaeea
Oleg <Wergweth@rambler.ru>
2023-07-25 21:09:16 +0300

    func delete and reverse added

--- main.py
+++ main.py
@@ -24,8 +24,36 @@ class LinkedList:
             while current.next is not None:
                 current = current.next
             current.next = new
+    def delete(self, value):
+        if self.head is None:
+            print("the list is empty")
+            return
+        if self.head.value == value:
+            self.head = self.head.next
+            print("deleted")
+            return
+        current = self.head
+        while current.next is not None:
+            if current.next.value == value:
+                current.next = current.next.next
+                print("deleted")
+                return
+            current = current.next
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
 
 ll = LinkedList()
 ll.insert(111)
 ll.insert(222)
+ll.insert(333)
+ll.display()
+ll.reverse()
 ll.display()
